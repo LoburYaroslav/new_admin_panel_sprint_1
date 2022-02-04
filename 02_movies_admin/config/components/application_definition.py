eb7f87cc2ce3b3cbd8f0b98b5f94d6fc4c2ddfea
Lobur Yaroslav <lobur.iar@yandex.ru>
2022-02-04 19:39:30 +0300

    добавил debug_toolbar

--- 02_movies_admin/config/components/application_definition.py
+++ 02_movies_admin/config/components/application_definition.py
@@ -7,7 +7,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'debug_toolbar',  # todo: мб опциональным сделать
+    'debug_toolbar',
     'movies.apps.MoviesConfig',
 ]
 
@@ -19,7 +19,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # 'debug_toolbar.middleware.DebugToolbarMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
